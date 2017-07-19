a887ec12a271401db071cf3054846609f0536917
fairy <fairy>
2017-07-19 15:52:36 +0800

    index

---
+++ bottom.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>{{showFlag}}</div>
+</template>
+<script>
+    export default {
+        props:['showFlag'],
+        data(){
+        return {
+            isShow:false
+        }
+    },
+
+    watch:{
+
+        showFlag:function(){
+            this.isShow=true;
+        }
+    }
+
+    }
+</script>
+
+
---
+++ index.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="shuhe-content">
+    <div class="nav-menu">
+        <el-col :span="24">
+            <el-menu default-active="2" class="el-menu-vertical-demo"  theme="dark" v-on:click.native="linkShow">
+                <el-submenu index="2">
+                    <template slot="title">水果</template>
+                    <li><router-link to="/index/map" v-on:click.native="warnShow">苹果</router-link></li>
+                    <li><router-link to="/index/map">西瓜</router-link></li>
+                </el-submenu>
+
+            </el-menu>
+        </el-col>
+    </div>
+    <router-view  v-bind:showFlag="showFlag" name="bottom"></router-view>
+</div>
+</template>
+<script type="text/ecmascript-6">
+    export default{
+        data(){
+            return {
+                showFlag:0,
+            }
+        },
+        methods: {
+            warnShow:function(){
+                if(this.showFlag>10){
+                    this.showFlag=0
+                }
+                this.showFlag++;
+            }
+        }
+    }
+</script>
+
